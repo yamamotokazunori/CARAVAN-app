06530cea97edc8bc7d31911a6eafd9701f982bd0
yamamotokazunori <gaogaigar673@gmail.com>
2021-04-01 05:22:38 +0000

    [add]投稿機能

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,8 +6,22 @@ class BlogsController < ApplicationController
   end
 
   def new
+    @blogs = Blog.new
+  end
+
+  def create
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
   end
 
   def edit
   end
+
+  private
+  def blog_params
+    params.require(:blog).premit(:title, :category, :body)
+  end
+
+
 end
